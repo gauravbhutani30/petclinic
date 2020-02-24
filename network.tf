f16e3154db092d20dd5c83a817fbde5629dd5a79
gauravbhutani30 <gauravbhutani30@gmail.com>
2020-02-24 20:57:13 +0530

    Network

---
+++ network.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
+}
+resource "aws_vpc" "terraformmain" {
+    cidr_block = "${var.vpc-fullcidr}"
+    # enable the internal vpc dns resolution
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags {
+        Name = "petclinic terraform vpc"
+    }
+}
